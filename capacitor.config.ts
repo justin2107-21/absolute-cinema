ef63fa42215feac1ec907e83c181be515323750e
justin2107-21 <justinesquita21@gmail.com>
2026-03-11 22:52:26 +0800

    CAPACTIOR

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.a076655157f9445e963f81551e521bcd',
+  appId: 'com.absolutecinema.app',
   appName: 'Absolute Cinema',
-  webDir: 'dist',
-  server: {
-    url: 'https://a0766551-57f9-445e-963f-81551e521bcd.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  webDir: 'dist'
 };
 
 export default config;
